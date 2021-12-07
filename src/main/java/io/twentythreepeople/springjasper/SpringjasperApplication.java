7eb4aa0f224691d15f2506a3e74a40c78a36adbc
Marcelo Ampuero <marcelo@23people.io>
2021-12-07 17:40:08 -0300

    add basic spring project

---
+++ src/main/java/io/twentythreepeople/springjasper/SpringjasperApplication.java
@@ -0,0 +1,13 @@
+package io.twentythreepeople.springjasper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjasperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjasperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/twentythreepeople/springjasper/SpringjasperApplicationTests.java
@@ -0,0 +1,13 @@
+package io.twentythreepeople.springjasper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjasperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
